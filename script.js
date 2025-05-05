474eb924d3ff1d44585d131d58a0121f6db56d31
Vova0211 <chernousov_07@bk.ru>
2025-05-05 17:21:48 +0300

    sort code

--- script.js
+++ script.js
@@ -8,8 +8,10 @@ function getTime(str) {
   let newStr = `${str.slice(0, 5)}`;
   return newStr;
 }
-
-function moreInfo(e) {
+function moreInfo(e, dop) {
+  if (dop == true) {
+    return `${e.target.dataset.value}`; 
+  }
   const div = document.createElement('div');
   div.classList.add("contact_info");
   if (e.target.classList[0] == "tel") {
@@ -38,136 +40,49 @@ async function delCont(id) {
 function confDelCont(id) {
   const back = document.createElement("div");
   back.classList.add("back");
-  const window = document.createElement("div");
-  window.classList.add("window");
-  const close = document.createElement('div');
-  close.classList.add("x");
-  window.appendChild(close);
   document.body.appendChild(back);
-  document.body.appendChild(window);
-  close.addEventListener("click", e => {
+  document.body.appendChild(document.getElementById("deleteWindow").content.cloneNode(true));
+  document.querySelector(".x").addEventListener("click", e => {
     document.querySelector(".back").remove();
     document.querySelector(".window").remove();
   });
-  const head = document.createElement("h2");
-  head.classList.add("window__head", "text-center");
-  head.textContent = "Новый клиент";
-  window.appendChild(head);
-  const descr = document.createElement('p');
-  descr.textContent = "Вы действительно хотите удалить данного клиента?";
-  descr.classList.add('text-center');
-  window.appendChild(descr);
-  const btnDel = document.createElement('button');
-  btnDel.classList.add("btn_create");
-  btnDel.textContent = "Удалить";
-  btnDel.addEventListener('click', e => {delCont(id)});
-  window.appendChild(btnDel);
-  const cancel = document.createElement('p');
-    cancel.classList.add("btn_cancel");
-    cancel.textContent = "Отмена";
-    cancel.addEventListener("click", e => {
-      document.querySelector(".back").remove();
-      document.querySelector(".window").remove();
-    });
-    window.appendChild(cancel);
+  document.querySelector(".btn_create").addEventListener('click', e => {delCont(id)});
+  document.querySelector('.btn_cancel').addEventListener("click", e => {
+    document.querySelector(".back").remove();
+    document.querySelector(".window").remove();
+  });
 }
 
 async function editWind(id) {
   const ans = await fetch(`http://localhost:3000/api/clients/${id}`);
   const data = await ans.json();
-  const window = document.querySelector('.window');
-  const head = document.createElement("h2");
+  document.querySelector(".window").appendChild(document.getElementById("editWindow").content.cloneNode(true));
   
-  const pId = document.createElement('p');
-  pId.classList.add("p_id");
-  pId.textContent = `ID: ${id}`;
-  const form = document.createElement("form");
-  const surname = document.createElement("input");
-  const name = document.createElement("input");
-  const lastname = document.createElement("input");
-  window.appendChild(head);
-  window.appendChild(pId);
-  form.classList.add("window__form");
-  head.classList.add("window__head");
-  head.textContent = "Изменить данные";
-  surname.placeholder = "Фамилия*";
-  surname.classList.add("surname");
-  name.classList.add("name*");
-  lastname.classList.add("lastname");
-  name.placeholder = "Имя";
-  lastname.placeholder = "Отчество";
-  surname.value = data.surname;
-  name.value = data.name;
-  lastname.value = data.lastName;
-    const add = document.createElement('div');
-    const addBtn = document.createElement('div');
-    addBtn.classList.add("add_btn")
-    add.classList.add("add_contact");
-    const h3 = document.createElement('h3');
-    h3.textContent = "Добавить контакт";
-    const addImg = document.createElement('img');
-    addImg.src = "img/create.svg";
-    add.appendChild(addBtn);
+  const surname = document.querySelector(".surname");
+  const name = document.querySelector(".name");
+  const lastname = document.querySelector(".lastname");
+    surname.value = data.surname;
+    name.value = data.name;
+    lastname.value = data.lastName;
+    const add = document.querySelector('.add_contact');
     for (let i = 0; i < data.contacts.length; i++) {
-      const divData = document.createElement('div');
-      divData.classList.add("data");
-      const select = document.createElement('select');
-      const input = document.createElement("input");
-      const delDiv = document.createElement('div');
-      delDiv.classList.add("del_div");
-      delDiv.addEventListener("click", e => {
-        divData.remove();
-      })
-      const img = document.createElement('img');
-      img.src = "img/cancel.svg";
-      delDiv.appendChild(img);
-      input.placeholder = "Введите данные контакта";
-      select.classList.add("select_contcts");
-      const options = document.getElementById("dataToContact").content.cloneNode(true);
-      select.appendChild(options);
-      select.value = data.contacts[i].type;
-      input.value = data.contacts[i].value;
-      divData.appendChild(select);
-      divData.appendChild(input);
-      divData.appendChild(delDiv);
+      const divData = document.getElementById("contactData").content.cloneNode(true);
+      divData.querySelector('select').value = data.contacts[i].type;
+      divData.querySelector('input').value = data.contacts[i].value;
+      divData.querySelector(".del_div").addEventListener("click", e => {e.target.parentNode.remove()})
       add.appendChild(divData);
     }
-    addBtn.addEventListener("click", e => {
-      const div = document.createElement('div');
-      div.classList.add("data");
-      const select = document.createElement('select');
-      const input = document.createElement("input");
-      const delDiv = document.createElement('div');
-      delDiv.classList.add("del_div");
-      delDiv.addEventListener("click", e => {
-        div.remove();
-      })
-      const img = document.createElement('img');
-      img.src = "img/cancel.svg";
-      delDiv.appendChild(img);
-      input.placeholder = "Введите данные контакта";
-      select.classList.add("select_contcts");
-      const options = document.getElementById("dataToContact").content.cloneNode(true);
-      select.appendChild(options);
-      div.appendChild(select);
-      div.appendChild(input);
-      div.appendChild(delDiv);
-      add.appendChild(div);
-    })
-    addBtn.appendChild(addImg);
-    addBtn.appendChild(h3);
-    const create = document.createElement('button');
-    create.textContent = "Сохранить";
-    create.classList.add("btn_create");
-    const cancel = document.createElement('p');
-    cancel.classList.add("btn_cancel");
-    cancel.textContent = "Удалить клиента";
-    cancel.addEventListener("click", e => {
+    document.querySelector(".add_btn").addEventListener("click", e => {
+      const contact = document.getElementById("contactData").content.cloneNode(true);
+      contact.querySelector(".del_div").addEventListener('click', e => {e.target.parentNode.remove()})
+      document.querySelector(".add_contact").appendChild(contact);
+    });
+    document.querySelector(".btn_cancel").addEventListener("click", e => {
       delCont(id);
       document.querySelector(".back").remove();
       document.querySelector(".window").remove();
     })
-    create.addEventListener('click', async (e) => {
+    document.querySelector(".btn_create").addEventListener('click', async (e) => {
       if (name.value.length == 0 || surname.value.length == 0) {
         const err = document.createElement('div');
         err.classList.add("error");
@@ -183,14 +98,6 @@ async function editWind(id) {
       })
       const ans = await fetch(`http://localhost:3000/api/clients/${id}`, {method: "PATCH", body: JSON.stringify(obj)});
     })
-
-    form.appendChild(surname);
-    form.appendChild(name);
-    form.appendChild(lastname);
-    window.appendChild(form);
-    window.appendChild(add);
-    window.appendChild(create);
-    window.appendChild(cancel);
 }
 
 function createWindow(type = "edit", elem) {
@@ -211,67 +118,20 @@ function createWindow(type = "edit", elem) {
     const id = elem.target.parentNode.parentNode.querySelector(".id").textContent;
     editWind(id);
   } else {
-    const head = document.createElement("h2");
-    head.classList.add("window__head");
-    head.textContent = "Новый клиент";
-    window.appendChild(head);
-    const form = document.createElement("form");
-    form.classList.add("window__form");
-    const surname = document.createElement("input");
-    const name = document.createElement("input");
-    const lastname = document.createElement("input");
-    // name.placeholder = `Имя`;
-    surname.classList.add("surname");
-    name.classList.add("name");
-    lastname.classList.add("lastname");
-    surname.placeholder = "Фамилия*";
-    name.placeholder = "Имя*";
-    lastname.placeholder = "Отчество";
-
-    const add = document.createElement('div');
-    const addBtn = document.createElement('div');
-    addBtn.classList.add("add_btn")
-    add.classList.add("add_contact");
-    const h3 = document.createElement('h3');
-    h3.textContent = "Добавить контакт";
-    const addImg = document.createElement('img');
-    addImg.src = "img/create.svg";
-    addBtn.addEventListener("click", e => {
-      const div = document.createElement('div');
-      div.classList.add("data");
-      const select = document.createElement('select');
-      const input = document.createElement("input");
-      const delDiv = document.createElement('div');
-      delDiv.classList.add("del_div");
-      delDiv.addEventListener("click", e => {
-        div.remove();
-      })
-      const img = document.createElement('img');
-      img.src = "img/cancel.svg";
-      delDiv.appendChild(img);
-      input.placeholder = "Введите данные контакта";
-      select.classList.add("select_contcts");
-      const options = document.getElementById("dataToContact").content.cloneNode(true);
-      select.appendChild(options);
-      div.appendChild(select);
-      div.appendChild(input);
-      div.appendChild(delDiv);
-      add.appendChild(div);
-    })
-    addBtn.appendChild(addImg);
-    addBtn.appendChild(h3);
-    add.appendChild(addBtn);
-    const create = document.createElement('button');
-    create.textContent = "Сохранить";
-    create.classList.add("btn_create");
-    const cancel = document.createElement('p');
-    cancel.classList.add("btn_cancel");
-    cancel.textContent = "Отмена";
-    cancel.addEventListener("click", e => {
+    document.querySelector(".window").appendChild(document.getElementById("createWindow").content.cloneNode(true));
+    const surname = document.querySelector(".surname");
+    const name = document.querySelector(".name");
+    const lastname = document.querySelector(".lastname");
+    document.querySelector(".add_btn").addEventListener("click", e => {
+      const contact = document.getElementById("contactData").content.cloneNode(true);
+      contact.querySelector(".del_div").addEventListener('click', e => {e.target.parentNode.remove()})
+      document.querySelector(".add_contact").appendChild(contact);
+    });
+    document.querySelector(".btn_cancel").addEventListener("click", e => {
       document.querySelector(".back").remove();
       document.querySelector(".window").remove();
     });
-    create.addEventListener('click', async (e) => {
+    document.querySelector(".btn_create").addEventListener('click', async (e) => {
       if (name.value.length == 0 || surname.value.length == 0) {
         const err = document.createElement('div');
         err.classList.add("error");
@@ -287,13 +147,6 @@ function createWindow(type = "edit", elem) {
       })
       const ans = await fetch("http://localhost:3000/api/clients", {method: "POST", body: JSON.stringify(obj)});
     })
-    form.appendChild(surname);
-    form.appendChild(name);
-    form.appendChild(lastname);
-    window.appendChild(form);
-    window.appendChild(add);
-    window.appendChild(create);
-    window.appendChild(cancel);
   }
 }
 
@@ -310,58 +163,6 @@ async function searchContact(value) {
   arr = arr.filter(a => res.includes(a.childNodes[1].textContent));
   document.querySelectorAll(".contact").forEach(e => {e.remove();});
   arr.forEach(el => {place.appendChild(el)});
-}
-
-function sortContacts() {
-  const place = document.querySelector(".main__load");
-  document.querySelector(".id_sort").parentNode.addEventListener('click', e => {
-    const arr = [...document.getElementsByClassName("contact")];
-    if (document.querySelector(".id_sort").src == "" || document.querySelector(".id_sort").src == "http://127.0.0.1:5500/img/arrow_up.svg") {
-      document.querySelector(".id_sort").src = "../img/arrow_down.svg";
-      arr.sort((a, b) => a.childNodes[1].textContent - b.childNodes[1].textContent)
-    } else {
-      document.querySelector(".id_sort").src = "../img/arrow_up.svg";
-      arr.sort((a, b) => b.childNodes[1].textContent - a.childNodes[1].textContent)
-    }
-    document.querySelectorAll(".contact").forEach(e => {e.remove();})
-    arr.forEach(el => {place.appendChild(el)});
-  })
-  document.querySelector(".fio_sort").parentNode.addEventListener('click', e => {
-    const arr = [...document.getElementsByClassName("contact")];
-    if (document.querySelector(".fio_sort").src == "" || document.querySelector(".fio_sort").src == "http://127.0.0.1:5500/img/arrow_up.svg") {
-      document.querySelector(".fio_sort").src = "../img/arrow_down.svg";
-      arr.sort((a, b) => a.childNodes[3].textContent.localeCompare(b.childNodes[3].textContent));
-    } else {
-      document.querySelector(".fio_sort").src = "../img/arrow_up.svg";
-      arr.sort((a, b) => b.childNodes[3].textContent.localeCompare(a.childNodes[3].textContent));
-    }
-    document.querySelectorAll(".contact").forEach(e => {e.remove();})
-    arr.forEach(el => {place.appendChild(el)});
-  })
-  document.querySelector(".create_sort").parentNode.addEventListener('click', e => {
-    const arr = [...document.getElementsByClassName("contact")];
-    if (document.querySelector(".create_sort").src == "" || document.querySelector(".create_sort").src == "http://127.0.0.1:5500/img/arrow_up.svg") {
-      document.querySelector(".create_sort").src = "../img/arrow_down.svg";
-      arr.sort((a, b) => Date.parse(a.childNodes[5].dataset.time) - Date.parse(b.childNodes[5].dataset.time))
-    } else {
-      document.querySelector(".create_sort").src = "../img/arrow_up.svg";
-      arr.sort((a, b) => Date.parse(b.childNodes[5].dataset.time) - Date.parse(a.childNodes[5].dataset.time))
-    }
-    document.querySelectorAll(".contact").forEach(e => {e.remove();})
-    arr.forEach(el => {place.appendChild(el)});
-  })
-  document.querySelector(".edit_sort").parentNode.addEventListener('click', e => {
-    const arr = [...document.getElementsByClassName("contact")];
-    if (document.querySelector(".edit_sort").src == "" || document.querySelector(".edit_sort").src == "http://127.0.0.1:5500/img/arrow_up.svg") {
-      document.querySelector(".edit_sort").src = "../img/arrow_down.svg";
-      arr.sort((a, b) => Date.parse(a.childNodes[7].dataset.time) - Date.parse(b.childNodes[7].dataset.time))
-    } else {
-      document.querySelector(".edit_sort").src = "../img/arrow_up.svg";
-      arr.sort((a, b) => Date.parse(b.childNodes[7].dataset.time) - Date.parse(a.childNodes[7].dataset.time))
-    }
-    document.querySelectorAll(".contact").forEach(e => {e.remove();})
-    arr.forEach(el => {place.appendChild(el)});
-  })
 }
 
 async function loadCards() {
@@ -395,6 +196,7 @@ async function loadCards() {
             contact.classList.add("contact_icon");
             contact.dataset.value = data[i].contacts[t].value;
             contact.addEventListener('mouseenter', moreInfo);
+            contact.addEventListener('click', e => {navigator.clipboard.writeText(moreInfo(e, true))});
             contact.addEventListener('mouseleave', delInfo);
             div.appendChild(contact);
           }
@@ -406,6 +208,7 @@ async function loadCards() {
       contact.classList.add("contact_icon");
       contact.dataset.value = data[i].contacts[j].value;
       contact.addEventListener('mouseenter', moreInfo);
+      contact.addEventListener('click', e => {navigator.clipboard.writeText(moreInfo(e, true))});
       contact.addEventListener('mouseleave', delInfo);
       temp.querySelector(".contacts").appendChild(contact);
     }
@@ -430,5 +233,5 @@ function start() {
 }
 loadCards()
 start()
-//Made by chatGPT
+// Made by chatGPT
 // Grazhdantcev is a god
---
+++ src/sort.js
@@ -0,0 +1,49 @@
+    const place = document.querySelector(".main__load");
+    document.querySelector(".id_sort").parentNode.addEventListener('click', e => {
+      const arr = [...document.getElementsByClassName("contact")];
+      if (document.querySelector(".id_sort").src == "" || document.querySelector(".id_sort").src == "http://127.0.0.1:5500/img/arrow_up.svg") {
+        document.querySelector(".id_sort").src = "../img/arrow_down.svg";
+        arr.sort((a, b) => a.childNodes[1].textContent - b.childNodes[1].textContent)
+      } else {
+        document.querySelector(".id_sort").src = "../img/arrow_up.svg";
+        arr.sort((a, b) => b.childNodes[1].textContent - a.childNodes[1].textContent)
+      }
+      document.querySelectorAll(".contact").forEach(e => {e.remove();})
+      arr.forEach(el => {place.appendChild(el)});
+    })
+    document.querySelector(".fio_sort").parentNode.addEventListener('click', e => {
+      const arr = [...document.getElementsByClassName("contact")];
+      if (document.querySelector(".fio_sort").src == "" || document.querySelector(".fio_sort").src == "http://127.0.0.1:5500/img/arrow_up.svg") {
+        document.querySelector(".fio_sort").src = "../img/arrow_down.svg";
+        arr.sort((a, b) => a.childNodes[3].textContent.localeCompare(b.childNodes[3].textContent));
+      } else {
+        document.querySelector(".fio_sort").src = "../img/arrow_up.svg";
+        arr.sort((a, b) => b.childNodes[3].textContent.localeCompare(a.childNodes[3].textContent));
+      }
+      document.querySelectorAll(".contact").forEach(e => {e.remove();})
+      arr.forEach(el => {place.appendChild(el)});
+    })
+    document.querySelector(".create_sort").parentNode.addEventListener('click', e => {
+      const arr = [...document.getElementsByClassName("contact")];
+      if (document.querySelector(".create_sort").src == "" || document.querySelector(".create_sort").src == "http://127.0.0.1:5500/img/arrow_up.svg") {
+        document.querySelector(".create_sort").src = "../img/arrow_down.svg";
+        arr.sort((a, b) => Date.parse(a.childNodes[5].dataset.time) - Date.parse(b.childNodes[5].dataset.time))
+      } else {
+        document.querySelector(".create_sort").src = "../img/arrow_up.svg";
+        arr.sort((a, b) => Date.parse(b.childNodes[5].dataset.time) - Date.parse(a.childNodes[5].dataset.time))
+      }
+      document.querySelectorAll(".contact").forEach(e => {e.remove();})
+      arr.forEach(el => {place.appendChild(el)});
+    })
+    document.querySelector(".edit_sort").parentNode.addEventListener('click', e => {
+      const arr = [...document.getElementsByClassName("contact")];
+      if (document.querySelector(".edit_sort").src == "" || document.querySelector(".edit_sort").src == "http://127.0.0.1:5500/img/arrow_up.svg") {
+        document.querySelector(".edit_sort").src = "../img/arrow_down.svg";
+        arr.sort((a, b) => Date.parse(a.childNodes[7].dataset.time) - Date.parse(b.childNodes[7].dataset.time))
+      } else {
+        document.querySelector(".edit_sort").src = "../img/arrow_up.svg";
+        arr.sort((a, b) => Date.parse(b.childNodes[7].dataset.time) - Date.parse(a.childNodes[7].dataset.time))
+      }
+      document.querySelectorAll(".contact").forEach(e => {e.remove();})
+      arr.forEach(el => {place.appendChild(el)});
+    })
